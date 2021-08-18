fa18e1aecb9b7d4b5466da41132fb2eee8b68faf
Pablo <pablo.esquivel@hotmail.com>
2021-08-18 07:10:21 -0300

    Change to scss stylesheets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import 'firebase/auth';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularGames';
@@ -22,28 +22,28 @@ export class AppComponent {
     };
     firebase.initializeApp(firebaseConfig);
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
+    // firebase
+    //   .auth()
+    //   .signInWithPopup(provider)
+    //   .then((result) => {
+    //     /** @type {firebase.auth.OAuthCredential} */
+    //     var credential = result.credential;
 
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential['accessToken'];
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    //     // This gives you a Google Access Token. You can use it to access the Google API.
+    //     var token = credential['accessToken'];
+    //     // The signed-in user info.
+    //     var user = result.user;
+    //     // ...
+    //   })
+    //   .catch((error) => {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
+    //     // ...
+    //   });
   }
 }
